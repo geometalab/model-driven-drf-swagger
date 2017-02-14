1089d0813bf865b101fdfd54043bf6c6533321a0
Nicola Jordan <nic@hixi.ch>
2017-02-14 20:16:53 +0100

    enable routes to posts and comments

--- app/app/urls.py
+++ app/app/urls.py
@@ -4,12 +4,12 @@ from rest_framework.routers import DefaultRouter
 from example import views
 
 router = DefaultRouter()
-# router.register(r'posts', views.PostViewSet)
-# router.register(r'comments', views.CommentViewSet)
+router.register(r'posts', views.PostViewSet)
+router.register(r'comments', views.CommentViewSet)
 router.register(r'users', views.UserViewSet)
 
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
